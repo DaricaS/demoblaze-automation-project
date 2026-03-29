cc34716f0e87d7f6daeb08ca36f45733b0df455b
DaricaS <daricastamkova11@gmail.com>
2026-03-29 19:23:52 +0200

    /

--- src/main/java/utils/ScreenshotUtil.java
+++ src/main/java/utils/ScreenshotUtil.java
@@ -7,30 +7,23 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class ScreenshotUtil {
 
-    public static String takeScreenshot(String testName) {
-
-        WebDriver driver = DriverManager.getDriver();
-
-        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String fileName = testName + "_" + timestamp + ".png";
-
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-        File destination = new File("screenshots/" + fileName);
+    public static void takeScreenshot(WebDriver driver, String testName) {
 
         try {
-            Files.createDirectories(destination.getParentFile().toPath());
-            Files.copy(screenshot.toPath(), destination.toPath());
+
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            File dest = new File("screenshots/" + testName + ".png");
+
+            Files.copy(src.toPath(), dest.toPath());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return destination.getAbsolutePath();
     }
 }
+
 
