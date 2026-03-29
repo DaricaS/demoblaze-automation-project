380170860f9740090c928cf90138efd227c39eae
DaricaS <daricastamkova11@gmail.com>
2026-03-29 17:30:13 +0200

    .

---
+++ src/main/java/listeners/TestListener.java
@@ -0,0 +1,31 @@
+package listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import utils.ScreenshotUtil;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
+
+        String screenshotPath = ScreenshotUtil.takeScreenshot(result.getName());
+        System.out.println("Screenshot saved at: " + screenshotPath);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
+}
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,19 @@
+package base;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverManager;
+
+public class BaseTest {
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        DriverManager.initDriver();
+        DriverManager.getDriver().get("https://www.demoblaze.com");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
